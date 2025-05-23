301f5a9a89f25bfa071874e125661a7a264a452b
Tarak1417 <achantatarak@gmail.com>
2025-05-23 07:25:25 +0530

    vf

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
-
+import Home from "./components/Home";
 import { useAppContext } from "./context/AppContext";
 import Sidebar from "./components/Sidebar";
 import CreateCampagin from "./components/CreateCampagin";
 import ParternerCampagin from "./components/ParternerCampagin";
 import Leaderboard from "./components/Leaderboard";
-import Home from "./components/home";
+
 
 function App() {
   const { adminEmail } = useAppContext();
